4ab08c3b6e94cdaae6bb550d96ce7e581055ee2a
Taha Yusuf Yıldız <tahayusufyildiz@Taha-Mac-mini.local>
2022-04-22 14:23:54 +0300

    .

---
+++ lib/data/questions_example.dart
@@ -0,0 +1,71 @@
+import 'package:quizz_app/model/question_model.dart';
+
+List<QuestionModel> questions = [
+  QuestionModel(
+    "Yusuf kaç yaşıdadır ?",
+    {
+      "22": false,
+      "23": false,
+      "25": true,
+      "9 aylık": false,
+    },
+  ),
+  QuestionModel("Hangisi RyseSoft çalışanı değildir ?", {
+    "Can": false,
+    "Enes": false,
+    "Alperen": false,
+    "Rukiye": true,
+  }),
+  QuestionModel("RyseSoft ofisi nerede yer almaktadır ?", {
+    "Keçiören": false,
+    "Sıhıye": true,
+    "Çinçin": false,
+    "Balgat": false,
+  }),
+  QuestionModel("Alperenin bilmediği dil hangisidir ?", {
+    "Türkçe": false,
+    "İngilizce": false,
+    "Arapça": true,
+    "İtalyanca": false,
+  }),
+  QuestionModel("Enesin adresi hangisinde doğru olarak verilmiştir ?", {
+    "Türbe caddesi türbe mahallesi": true,
+    "Türbe caddesi mevlana mahallesi": false,
+    "Semazen mahallesi türbe sokak": false,
+    "Konya ovası": false,
+  }),
+  QuestionModel("Projenin ismi nedir ?", {
+    "SınavKutusu.com": false,
+    "DenemeSınavı.com": false,
+    "Sınavsepeti.com": true,
+    "Sınavcepte.com": false,
+  }),
+  QuestionModel(
+      "Aşağıdaki üniversitelerden en çok hangisinden uzak durulmaıdır ?", {
+    "Nuh Naci Yazgan": false,
+    "Cumhuriyet": false,
+    "Kırıkkale": false,
+    "Erciyes": true,
+  }),
+  QuestionModel("Alperen'in en sevdiği araç nedir ?", {
+    "Kamyon": false,
+    "Motor": false,
+    "Kepçe": true,
+    "Otobüs": false,
+  }),
+  QuestionModel(
+      "ELEYİCİ SORU/ Ofiste kaç adet monitör vardır ?",
+      {
+        "7": false,
+        "6": false,
+        "8": true,
+        "9": false,
+      }),
+  QuestionModel(
+      "Herkesi güldüren palyonço aslında kimdir? ?", {
+    "Doktor": false,
+    "Kendisi": true,
+    "Mahalle muhtarı": false,
+    "Yusuf": false,
+  }),
+];
---
+++ lib/screens/main_menu.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:quizz_app/screens/quizz_screen.dart';
+import 'package:quizz_app/ui/shared/color.dart';
+
+class MainMenu extends StatefulWidget {
+  const MainMenu({Key? key}) : super(key: key);
+
+  @override
+  _MainMenuState createState() => _MainMenuState();
+}
+
+class _MainMenuState extends State<MainMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColor.pripmaryColor,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 48.0,
+          horizontal: 40.0,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Spacer(flex:1,),
+           
+            const Center(
+              child: Text(
+                
+                "MYK DENEME SINAVINA HOŞGELDİNİZ",
+                style: TextStyle(
+                  color: Colors.purple,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Expanded(
+              child: Center(
+                child: RawMaterialButton(
+                  onPressed: () {
+                    //Navigating the the Quizz Screen
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => QuizzScreen(),
+                        ));
+                  },
+                  shape: const StadiumBorder(),
+                  fillColor: Colors.purple,
+                  child: const Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
+                    child: Text(
+                      "Sınavı Başlat",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 26.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+           
+          ],
+        ),
+      ),
+    );
+  }
+}
